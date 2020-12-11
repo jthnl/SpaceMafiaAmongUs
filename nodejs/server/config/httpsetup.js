0de907a743379f8bbe0fefd29b0d3885c22ff00b
Jathniel Ong <ong.jathniel@gmail.com>
2020-12-11 00:07:28 -0700

    Fix Sign Up Bug
    Authenticate user on successful sign-up, was previously unsuccessful in
    Authenticating user. Should probably consider using Moogoose.

--- nodejs/server/config/httpsetup.js
+++ nodejs/server/config/httpsetup.js
@@ -27,6 +27,7 @@ module.exports = function (app, db) {
         // attempt to authenticate passport 
         passport.authenticate('local', { failWithError: true }),
         function (req, res, next) {
+            console.log(JSON.stringify(req.user));
             // if successfully authenticated, return sucess JSON object
             res.json({
                 "username": req.body.username,
@@ -49,64 +50,47 @@ module.exports = function (app, db) {
      * Expects JSON Object: { username : "<username>", password : "<password>", email: "<email>", name: "<name>"}
      * Returns JSON Object: { username : "<username>", success : true/false, message: "<error message>" }
      */
-    app.route('/register').post(
-        (req, res, next) => {
-            addNewUser(req, res, next);
-        },
-        passport.authenticate('local', { failWithError: true }),
-        function (req, res, next) {
-            console.log("successfully registered User");
-        },
-        function (err, req, res, next) {
-            console.log("register: something went wrong");
-            next(err);
-        }
-    );
-
-
-    /**
-     * Helper function to Add user to DB 
-     * FUTURE TODO: clean up code using Mongoose
-     */
-    function addNewUser(req, res, next) {
-        let query = {
-            $or: [
-                { "username": req.body.username },
-                { "email": req.body.email }
-            ]
-        };
+    app.post('/register', (req, res, next) => {
         let newUser = {
             "username": req.body.username,
             "password": req.body.password,
             "email": req.body.email,
             "name": req.body.name,
             "gamesplayed": 0,
-            "wins":0,
-            "losses":0
-        }
+            "wins": 0,
+            "losses": 0
+        };
+
+        let query = {
+            $or: [
+                { "username": req.body.username },
+                { "email": req.body.email }
+            ]
+        };
+
         db.findOne(query, function (err, user) {
             if (err) {
                 next(err);
             } else if (user) {
-                // if user already exists, return JSON with error
                 res.json(jsonMessageBuilder(req.body.username, false, "username or email is already in use"));
             } else {
                 // create a new user and insert to database
                 db.insertOne(newUser, (err, doc) => {
                     if (err) {
-                        // if insert fails
-                        console.log(console.error());
-                        res.json(jsonMessageBuilder(req.body.username, false, "was not able to create new user"));
+                        res.json(jsonMessageBuilder(req.body.username, false, "unable to add user, please try again."));
                     } else {
-                        // if successfully inserted, return success message
-                        console.log(doc.ops[0]);
-                        res.json(jsonMessageBuilder(req.body.username, true, "successfully created new user"));
+                        req.login(doc.ops[0], function (err) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                res.send({ success: "success" });
+                            }
+                        });
                     }
                 });
             }
-        }
-        );
-    }
+        });
+    });
 
     function jsonMessageBuilder(username, success, message) {
         return {
